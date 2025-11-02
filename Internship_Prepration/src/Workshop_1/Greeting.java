26f7bb645f0c1990b7ed7763e36ae3991be97c40
Samarth <samarthswami96051@gmail.com>
2025-11-02 13:00:12 +0530

    Greeting to the User

--- Internship_Prepration/src/Workshop_1/Greeting.java
+++ Internship_Prepration/src/Workshop_1/Greeting.java
@@ -1,8 +1,29 @@
-package Workshop_1;
-import java.util.*;
+// package CodingWorkshop2;
+
+import java.util.Scanner;
+
 public class Greeting {
-    public static void main(String args[]) {
-        System.out.println("Welcome to the  java ");
+
+    //Function simpleGreetings to accept name as input
+    // and print simple greeting "Namaskara Karnataka" name
+
+    public static void simpleGreeting(String name){
+
+        System.out.println("The name Entered by the User : " +name);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Enter the name : ");
+        String name = obj.nextLine();
+
+        System.out.println();
+
+        simpleGreeting(name);
+
+        obj.close();
 
     }
 }
