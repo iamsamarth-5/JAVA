cdc56f6fb5f1d4ec0673d145735df06c2dc60a4d
Samarth <samarthswami96051@gmail.com>
2025-11-02 13:01:47 +0530

    Sum of Two Number

---
+++ Internship_Prepration/src/Workshop_1/pgm_1.java
@@ -0,0 +1,21 @@
+package Workshop_1;
+
+public class pgm_1 {
+    //Function getSum to get Sum of numbers
+
+    public static int getSum(int num1, int num2){
+        // int sum = 0;
+        // sum = num1 + num2;
+        return num1 + num2;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("The sum of two numbers : " +getSum(10, 20));
+
+    }
+
+}
+
+
+
