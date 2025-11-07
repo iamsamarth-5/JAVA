eae993a4fd10ab6a476f922bb5f33b4f255f4433
Samarth <samarthswami96051@gmail.com>
2025-11-07 23:43:00 +0530

    PrintStarsIndiffWay

--- Internship_Prepration/LoopsPart_2/src/LoopsAdvance.java
+++ Internship_Prepration/LoopsPart_2/src/LoopsAdvance.java
@@ -1,40 +1,113 @@
 import java.util.ArrayList;
 
-public class LoopsAdvance {
-    public static void main(String[] args) {
-        // print100Stars(10000);
-        // printStarGrid(5);
-        // int[] numbers = {1, 5, 8, 10, 11, 12};
-        // printStarsForArray(numbers);
-        // PrintPyramid(5);
-        PrintInvertedPyramid(5);
-    }
+public class LoopsAdvanced {
 
-    public static void print100Stars(int countOfStars) {
-        for (int count = 1; count <= countOfStars; count++) {
-            System.out.print("* ");
+    public static void Print100Stars(int countOfStars)
+    {
+        for (int count=1; count <= countOfStars; count++)
+        {
+            System.out.print("*");
         }
     }
 
-    public static void printStarGrid(int gridSize) {
-        for (int row = 1; row <= gridSize; row++) {
-            for (int col = 1; col <= gridSize; col++) {
-                System.out.print("* ");
+    public static void PrintStarGrid(int gridSize)
+    {
+
+        for (int count=1; count <= gridSize; count++)
+        {
+            for (int row=1; row<= gridSize; row++)
+            {
+                System.out.print("*");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 
-    public static void PrintPyramid(int height) {
-        int countOfStars = 1;
-        for (int level = 1; level <= height; level++) {
-            int noOfSpace = height - level;
+    public static void PrintStarGridConcatApproach(int gridSize)
+    {
+        for (int count=1; count <= gridSize; count++)
+        {
             StringBuilder line = new StringBuilder();
+            for (int row=1; row<= gridSize; row++)
+            {
+                line.append("*");
+            }
+            System.out.println(line.toString());
+        }
+    }
 
-            for (int count = 0; count < noOfSpace; count++) {
+    public static void printStarsForArray(int[] numbers)
+    {
+        for (int index=0; index < numbers.length; index++) // To read elements from the array
+        {
+            for (int count=1; count <= numbers[index] ; count++ ) // To print *
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void PrintStarGridSkipEven(int gridSize)
+    {
+        for (int count=1; count <= gridSize; count++)
+        {
+            StringBuilder line = new StringBuilder();
+            for (int row=1; row<= gridSize; row++)
+            {
+                // How to check if row value is even
+                /*
+                if (row % 2 == 0)
+                {
+                    line.append(" ");
+                }
+                else
+                {
+                    line.append("*");
+                } */
+                line.append(row % 2 == 0 ? " " : "*");
+            }
+            System.out.println(line.toString());
+        }
+    }
+
+    public static void PrintStarsOnlyAtBorders(int gridSize)
+    {
+        for (int row=1; row <= gridSize; row++)
+        {
+            StringBuilder line = new StringBuilder();
+            for (int column=1; column <= gridSize; column++)
+            {
+                /*if (row == 1 || row == gridSize || column == 1 || column == gridSize)
+                {
+                    line.append("* ");
+                }
+                else
+                {
+                    line.append("  ");
+                }*/
+
+                Boolean isStar = row == 1 || row == gridSize || column == 1 || column == gridSize;
+                line.append(isStar ? "* ": "  ");
+            }
+            System.out.println(line.toString());
+        }
+    }
+
+    public static void PrintPyramid(int height)
+    {
+        int countOfStars = 1;
+        for (int level=1; level <= height; level++)
+        {
+            int noOfSpace = height-level;
+            StringBuilder line = new StringBuilder();
+            for (int count=0; count<noOfSpace; count++)
+            {
                 line.append(" ");
             }
-            for (int count = 0; count < countOfStars; count++) {
+
+            for (int count=0; count<countOfStars; count++)
+            {
                 line.append("*");
             }
 
@@ -43,16 +116,22 @@ public class LoopsAdvance {
         }
     }
 
-    public static void PrintInvertedPyramid(int height) {
-        for (int level = 1; level <= height; level++) {
-            int noOfSpaces = level - 1;
-            int noOfStars = (height - level) * 2 + 1;
-            StringBuilder line = new StringBuilder();
+    public static void PrintInvertedPyramid(int height)
+    {
 
-            for (int count = 0; count < noOfSpaces; count++) {
+        for (int level=1; level <= height; level++)
+        {
+            int noOfSpace = level - 1;
+            int noOfStars = ( height - level ) * 2 + 1;
+
+            StringBuilder line = new StringBuilder();
+            for (int count=0; count<noOfSpace; count++)
+            {
                 line.append(" ");
             }
-            for (int count = 0; count < noOfStars; count++) {
+
+            for (int count=0; count<noOfStars; count++)
+            {
                 line.append("*");
             }
 
@@ -60,12 +139,19 @@ public class LoopsAdvance {
         }
     }
 
-    public static void printStarsForArray(int[] numbers) {
-        for (int index = 0; index < numbers.length; index++) {
-            for (int count = 1; count <= numbers[index]; count++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+    public static void main(String[] args) {
+        PrintInvertedPyramid(10);
+        PrintPyramid(10);
+        //PrintStarsOnlyAtBorders(10);
+
+        //Print100Stars(10000);
+        // PrintStarGrid(5);
+
+        //int[] numbers = { 1,5,8,10,4,6,9,10,11,12};
+        //printStarsForArray(numbers);
+
+        //PrintStarGridConcatApproach(5);
+
+        //PrintStarGridSkipEven(5);
     }
 }
