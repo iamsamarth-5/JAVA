53bdcd4afe7d5c3c3a1d53bd1ef03d16e6a666c7
Samarth <samarthswami96051@gmail.com>
2025-11-13 01:11:44 +0530

    createing a object for the SinglyLinkedList  ot use the method and data present in the class

---
+++ Internship_Prepration/DSA/src/DSA_Application.java
@@ -0,0 +1,13 @@
+public class DSA_Application {
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList Samarth =  new SinglyLinkedList();// here we createing a object for the SinglyLinkedList  ot use the method and data present in the class
+        //singlyLinkedList.testAllOperations();
+
+        //singlyLinkedList.testInsertAtPosition();
+
+        Samarth.testDeleteAtPosition();
+        Internship_Prepration/DSA/src/DSA_Application.java
+    }
+}
